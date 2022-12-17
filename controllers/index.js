3cf0afccb4c400c2551278ded966cb26f2320a5f
Haley May <haymon24u@gmail.com>
2022-12-16 14:21:15 -1000

    sets up server connection and api connection

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
-
-router.use('/api', apiRoutes);
-
+// const apiRoutes = require('./api');
+const templateRoutes = require('./templates')
+// router.use('/api', apiRoutes);
+router.use('/', templateRoutes);
 module.exports = router;
